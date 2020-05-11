4d089120dca4139b3609653fd3b68b2043d03b38
Stanislav Levchenko <staslevchenko@protonmail.com>
2020-05-11 20:11:21 +0300

    The second commit - README file was added for better structure; - code in avg_salary_stat.py was formated according to PEP8 standarts; - some `else` structures were deleted; - two functions, which counted average salary for different sites were combined in one; - creation of dictionaries became more simple; - data types were removed from variable names; - all global variables were removed from functions body; - variable names were specified.

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 This project counts average salary for different programming languages developers, devopses and data science specialists in different cities or regions.
 
 #### Dependences
-You need Python3 interpreter to use this script. All needed dependences are included in requirements.txt file. Use ```pip install -r requirements.txt``` to install them (use ```pip3``` instead ```pip```  if you have both python2 and python3 versions).
+You need Python3 interpreter to use this script. All needed dependences are included in requirements.txt file. Use 
+```
+pip install -r requirements.txt
+``` 
+to install them (use `pip3` instead `pip`  if you have both python2 and python3 versions).
 
 #### Usage
-Run avg_salary_stat.py  and give it the name of city or region as an argument (only cirillic symbols). As a result you will get two tables. The first one will show you average salary statistics according to hh.ru resource for the last 30 days. And the second one will show you the same statistics according to superjob.ru for the last 7 days. This script processes vacancies with specified salary in RUR currency.
+Run the script  and give it the name of city or region as an argument (only cirillic symbols)
+```
+python avg_salary_stat.py Москва
+```
+use `python3` instead `python`  if you have both python2 and python3 versions
+As a result you will get two tables. The first one will show you average salary statistics according to hh.ru resource for the last 30 days. And the second one will show you the same statistics according to superjob.ru for the last 7 days. This script processes vacancies with specified salary in RUR currency.
 
 #### Purpose of project
 This script was performed as a part of API web-services cource by [Devman](https://dvmn.org/modules)
--- avg_salary_stat.py
+++ avg_salary_stat.py
@@ -4,58 +4,67 @@ from dotenv import load_dotenv
 import argparse
 import os
 
-HH_URL = 'https://api.hh.ru/vacancies/'
-SJ_URL = 'https://api.superjob.ru/2.30/vacancies'
 
-LANGUAGES = ['C', 'C++', 'C#', 'Python','Java', 'Javascript','Ruby', 'Scala','Go', '1C', 'PHP', 'DevOps','Data Analyst','Data Scientist']
+LANGUAGES = [
+    'C',
+    'C++',
+    'C#',
+    'Python',
+    'Java',
+    'Javascript',
+    'Ruby',
+    'Scala',
+    'Go',
+    '1C',
+    'PHP',
+    'DevOps',
+    'Data Analyst',
+    'Data Scientist'
+    ]
 
-def get_city_id_hh (city_name):
-    city_response = requests.get('https://api.hh.ru/areas')
+
+def get_city_id_hh(url, city_name):
+    city_response = requests.get(url)
     locations = city_response.json()
     for country in locations:
         if city_name in country.values():
             return country['id']
-        else:
-            for region in country['areas']:
-                if city_name in region.values():
-                    return region['id']
-                else:
-                    for city in region['areas']:
-                        if city_name in city.values():
-                            return city['id']
-
-
-def get_city_id_sj (city_name):
-    city_response = requests.get('https://api.superjob.ru/2.30/regions/combined', headers = sj_headers)
-    locations = city_response.json()
-    for country in locations:
-        if not 'title' in country.keys():
-            continue
-        else:
-            for city in country['towns']:
+        for region in country['areas']:
+            if city_name in region.values():
+                return region['id']
+            for city in region['areas']:
                 if city_name in city.values():
                     return city['id']
-            if not 'regions' in country.keys():
-                continue
-            else:
-                for region in country['regions']:
-                    if city_name == region['title']:
-                        return region['id']
-                    else:
-                        for town in region['towns']:
-                            if city_name in town.values():
-                                return town['id']
-                    
 
-def get_hh_vacancies (params):
-    
+
+def get_city_id_sj(url, headers, city_name):
+    city_response = requests.get(url, headers=headers)
+    locations = city_response.json()
+    for country in locations:
+        if 'title' not in country.keys():
+            continue
+        for city in country['towns']:
+            if city_name in city.values():
+                return city['id']
+            if 'regions' not in country.keys():
+                continue
+            for region in country['regions']:
+                if city_name == region['title']:
+                    return region['id']
+                for town in region['towns']:
+                    if city_name in town.values():
+                        return town['id']
+
+
+def get_hh_vacancies(url, params):
+
     vacancies = []
     page = 0
     pages = 1
-    
+
     while page < pages:
         params['page'] = page
-        response = requests.get(HH_URL, params=params)
+        response = requests.get(url, params=params)
         response.raise_for_status()
         answer = response.json()
         for item in answer['items']:
@@ -64,34 +73,38 @@ def get_hh_vacancies (params):
         pages = answer['pages']
     return vacancies
 
-def get_salary_statistics_hh (language_list):
+
+def get_salary_statistics_hh(vacancy_url, areas_url, languages):
     statistics = {}
-    for language in language_list:
+    for language in languages:
         request_params = {
-            'text' : f'{language}',
-            'area' : get_city_id_hh (city_name),
-            'period' : 30,
-            'only_with_salary' : True
+            'text': language,
+            'area': get_city_id_hh(areas_url, city_name),
+            'period': 30,
+            'only_with_salary': True
         }
-        
-        answer = get_hh_vacancies(request_params)
+
+        answer = get_hh_vacancies(vacancy_url, request_params)
         number_and_avg_salary = count_avg_language_salary_hh(answer)
         vacancies_count = len(answer)
         vacancies_processed = number_and_avg_salary[0]
         avg_salary = number_and_avg_salary[1]
-        statistics[language]={
+        statistics[language] = {
             'vacancies_count': vacancies_count,
-            'vacancies_processed' : vacancies_processed,
-            'average_salary' : avg_salary
+            'vacancies_processed': vacancies_processed,
+            'average_salary': avg_salary
         }
     return statistics
 
-def count_avg_language_salary_hh (response):
+
+def count_avg_language_salary_hh(response):
     avg_salaries = []
     for item in response:
         if not item['salary']['currency'] == 'RUR':
             continue
-        avg_vacancy_salary = count_avg_position_salary_hh (item)
+        salary_from = item['salary']['from']
+        salary_to = item['salary']['to']
+        avg_vacancy_salary = count_avg_salary(salary_from, salary_to)
         avg_salaries.append(avg_vacancy_salary)
     processed_vacansies = len(avg_salaries)
     if processed_vacansies:
@@ -100,10 +113,9 @@ def count_avg_language_salary_hh (response):
         avg_language_salary = 0
     language_stats = [processed_vacansies, avg_language_salary]
     return language_stats
-        
-def count_avg_position_salary_hh (vacancy):
-    start = vacancy['salary']['from']
-    end = vacancy['salary']['to']
+
+
+def count_avg_salary(start, end):
     if not start:
         avg_salary = end * 0.8
     elif not end:
@@ -111,90 +123,90 @@ def count_avg_position_salary_hh (vacancy):
     else:
         avg_salary = (end + start) / 2
     return avg_salary
-   
-def get_sj_vacancies(params):
+
+
+def get_sj_vacancies(url, headers, params):
     vacancies = []
     page = 0
     more = True
-    
+
     while more:
         params['page'] = page
-        response = requests.get(SJ_URL, headers = sj_headers, params=params)
+        response = requests.get(url, headers=headers, params=params)
         response.raise_for_status()
         answer = response.json()
-        for vacancy in answer ['objects']:
+        for vacancy in answer['objects']:
             vacancies.append(vacancy)
         page += 1
         more = answer['more']
     return vacancies
 
-def count_avg_language_salary_sj (vacancy):
-    start = vacancy ['payment_from']
-    end = vacancy ['payment_to']
-    if not end:
-        avg_salary = start * 1.2
-    elif not start:
-        avg_salary = end * 0.8
-    else:
-        avg_salary = (start + end) / 2
-    return avg_salary
 
-def get_salary_statistics_sj (languages_list):
+def get_salary_statistics_sj(vacancy_url, areas_url, headers, languages):
     sj_statistics = {}
-    for language in languages_list:
+    for language in languages:
         sj_params = {
-            'catalogues' : 48,
-            'town' : get_city_id_sj (city_name),
-            'period' : 7,
-            'keywords[0][srws]' : 1,
-            'keywords[0][keys]' : f'{language}'
+            'catalogues': 48,
+            'town': get_city_id_sj(areas_url, headers, city_name),
+            'period': 7,
+            'keywords[0][srws]': 1,
+            'keywords[0][keys]': language
             }
-        sj_statistics [f'{language}'] = {}
-        response = get_sj_vacancies(sj_params)
+        sj_statistics[language] = {}
+        response = get_sj_vacancies(vacancy_url, headers, sj_params)
         total_vacancies = len(response)
         processed_vacancies = []
         for vacancy in response:
-            if (vacancy ['payment_from'] or vacancy ['payment_to']) and vacancy ['currency'] == 'rub':
-                avg_salary = count_avg_language_salary_sj (vacancy)
+            if (vacancy['payment_from'] or vacancy['payment_to']) and vacancy['currency'] == 'rub':
+                salary_from = vacancy['payment_from']
+                salary_to = vacancy['payment_to']
+                avg_salary = count_avg_salary(salary_from, salary_to)
                 processed_vacancies.append(avg_salary)
             else:
                 continue
-        total_processed = len (processed_vacancies)
+        total_processed = len(processed_vacancies)
         if total_processed:
             total_avg_salary = sum(processed_vacancies) / total_processed
         else:
             total_avg_salary = 0
-    
-        sj_statistics [f'{language}']['vacancies_count'] = total_vacancies
-        sj_statistics [f'{language}'] ['vacancies_processed'] = total_processed
-        sj_statistics [f'{language}'] ['average_salary'] = int(total_avg_salary)
-    
+
+        sj_statistics[language] = {
+            'vacancies_count': total_vacancies,
+            'vacancies_processed': total_processed,
+            'average_salary': int(total_avg_salary)
+        }
+
     return sj_statistics
 
-def make_terminal_table(dictionary, title):
-    table_list = [['Language','Vacancies count','Vacancies processed','Average salary']]
-    for key, value in dictionary.items():
-        language_list = []
-        language_list.append(key)
-        for value in value.values():
-            language_list.append(value)
-        table_list.append(language_list)
-    terminal_table = DoubleTable(table_list, title = title)
+
+def make_terminal_table(statistics, title):
+    table = [['Language', 'Vacancies count', 'Vacancies processed', 'Average salary']]
+    for language, category in statistics.items():
+        languages = []
+        languages.append(language)
+        for count in category.values():
+            languages.append(count)
+        table.append(languages)
+    terminal_table = DoubleTable(table, title=title)
     return terminal_table.table
 
 if __name__ == '__main__':
     load_dotenv()
     sj_token = os.getenv('SJ_TOKEN')
-    sj_headers = {'X-Api-App-Id' : sj_token}
+    sj_headers = {'X-Api-App-Id': sj_token}
+
+    hh_vacancy_url = 'https://api.hh.ru/vacancies/'
+    sj_vacancy_url = 'https://api.superjob.ru/2.30/vacancies'
+    hh_areas_url = 'https://api.hh.ru/areas'
+    sj_areas_url = 'https://api.superjob.ru/2.30/regions/combined'
 
     parser = argparse.ArgumentParser(description='show average salary for different languages developers')
     parser.add_argument('city_name', help='input city name in cirilic from the capital letter')
     args = parser.parse_args()
     city_name = args.city_name
 
-    hh = get_salary_statistics_hh (LANGUAGES)
-    sj = get_salary_statistics_sj (LANGUAGES)
+    hh = get_salary_statistics_hh(hh_vacancy_url, hh_areas_url, LANGUAGES)
+    sj = get_salary_statistics_sj(sj_vacancy_url, sj_areas_url, sj_headers, LANGUAGES)
     print(make_terminal_table(hh, 'Head Hunter vacancies'))
     print(make_terminal_table(sj, 'Super Job vacancies'))
-    
 
